03b53a5767cbec9e9249ebff048e887732805def
ytisdel <93150405+ytisdel@users.noreply.github.com>
2021-12-19 07:36:00 -0700

    created a function to call the api to retrieve the current daily forecast.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,14 @@ let day = days[now.getDay()];
 
 h5.innerHTML = `${day} ${hour}:${minutes}`;
 
-function displayForecast() {
+function getForecast(coordinates) {
+  let apiKey = "734f282e7f8aa78f2c9c1a2c6230861d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -57,7 +64,7 @@ function showCurrentTemperature(response) {
 
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   temperatureMain.innerHTML = `${temperature}`;
   windSpeed.innerHTML = `Wind: ${wind} Km/h`;
@@ -71,6 +78,8 @@ function showCurrentTemperature(response) {
   } else {
     airQuality.innerHTML = `Air Quality: Bad 👎`;
   }
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -135,4 +144,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showcelsiusTemperature);
 
 searchCity("Phoenix");
-displayForecast();
